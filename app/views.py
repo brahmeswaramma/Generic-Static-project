131d472e4d3bf4b24cf1a930a3a965d90deb15ca
brahmeswaramma <brahmeswarammaathinti@gamil.com>
2023-12-28 11:47:59 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def one(request):
+    return render(request,'one.html')
+def two(request):
+    return render(request,'two.html')
+def three(request):
+    return render(request,'three.html')
